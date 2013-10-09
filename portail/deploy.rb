2e341ef43b21e20f1f89444fa291146e80dab719
Jean-Baptiste Delhommeau <jbdelhommeau@naoned-systemes.fr>
2013-10-09 16:16:04 +0200

    Update deploy.rb

--- portail/deploy.rb
+++ portail/deploy.rb
@@ -1,62 +1,52 @@
 set  :application,            "mnesys_portail"
-set  :domain,                 "pv2-01.naoned-systemes.fr"
-set  :deploy_to,              "/home/inao/mnesys_portail"
-
-set :mailgun_api_key, 'key-79c42qx4eqypyx-jjxr6s0kkwgqhe9x4' # your mailgun API key
-set :mailgun_domain, 'naonedsystemes.mailgun.org' # your mailgun email domain
-set :mailgun_from, 'deploymement_prod@mnesys.fr' # who the email will appear to come from
-set :mailgun_recipients, [ 'tous@naoned-systemes.fr' ] # who will receive the email
+set  :domain,                 "localhost"
+set  :deploy_to,              "/home/www/inao/mnesys_portail"
 
 set  :scm,                    :git
-#set  :git_enable_submodules,  1
 set  :repository,             "git@github.com:naonedsystemes/mnesys_portail.git"
-set :branch, "stable"
+set  :branch, "stable"
 #set  :deploy_via,             :remote_cache
-set :deploy_via, :rsync_with_remote_cache
+set :deploy_via, 			        :rsync_with_remote_cache
 
 role :web,                    domain
 role :app,                    domain
 role :db,                     domain, :primary => true
 
-set  :user,                   "root"				
+set  :user,                   "root"
 set  :use_sudo,               false
 set  :keep_releases,          3
 ssh_options[:forward_agent] = true
 
 set :php, "/usr/bin/php"
-
 set :cake_app, "app"
-
 
 namespace (:deploy) do
 
   desc <<-DESC
-    [internal] Overriding original task to fit to symfony project needs
+    [internal] Overriding original task to fit to cakephp project needs
   DESC
   task :finalize_update, :except => { :no_release => true } do
     # Fix permissions
     run "cd #{latest_release} && chmod 775 -R *;"
     run "cd #{latest_release} && chown www-data:www-data -R *;"
-  
   end
-  
+
   desc <<-DESC
     Overriding original task to exclude restart
   DESC
   task :default do
     update
   end
-  
+
   desc <<-DESC
-    Overriding original task to use symfony migrations
+    Overriding original task to use cakephp migrations
   DESC
   task :migrations do
     update
-    #sf.migrate
-  end  
+    #ca.migrate
+  end
   
-  after "deploy:update", 'deploy:customize'
-  after :deploy, 'mailgun_notify'
+  after "deploy:update", "deploy:customize", "deploy:cleanup"
   
   desc <<-DESC
     Custom tasks
@@ -65,6 +55,8 @@ namespace (:deploy) do
     ca.symlinks
     ca.cc
     ca.rights
+    ca.acl
+    system 'cap apache:reload'
   end
  
 end
@@ -73,7 +65,7 @@ namespace :apache do
   [:stop, :start, :restart, :reload].each do |action|
     desc "#{action.to_s.capitalize} Apache"
     task action, :roles => :web do
-      invoke_command "/etc/init.d/apache2 #{action.to_s}", :via => run_method
+    	invoke_command "/etc/init.d/apache2 #{action.to_s}", :via => run_method
     end
   end
 end
@@ -86,6 +78,13 @@ namespace (:ca) do
   task :cc do
     run "cd #{current_path} && rm -rf app/tmp/cache/models/*"
     run "cd #{current_path} && rm -rf app/tmp/cache/persistent/*"
+  end
+
+ desc <<-DESC
+    Run the "cake ACL" task
+  DESC
+  task :acl do
+    run "cd #{current_path}/#{cake_app} && ./Console/cake AclExtras.AclExtras aco_sync;"
   end
 
  desc <<-DESC
@@ -111,11 +110,13 @@ namespace (:ca) do
 
     # symlink to uploads
     run "rm -rf #{current_path}/app/webroot/uploads"
-    run "ln -s /bigfs/uploads #{current_path}/app/webroot/uploads"
+    run "ln -s /home/www/inao/uploads #{current_path}/app/webroot/uploads"
+    #run "ln -s {inao_datas_mnesys_portail}/uploads #{current_path}/app/webroot/uploads"
     
     # symlink to medias
     run "rm -rf #{current_path}/app/webroot/medias"
-    run "ln -s /bigfs/medias #{current_path}/app/webroot/medias"
+    run "ln -s /home/www/inao/medias #{current_path}/app/webroot/medias"
+    #run "ln -s {inao_datas_mnesys_portail}/medias #{current_path}/app/webroot/medias"
 
     # symlink to copy_instance
     run "rm -rf #{current_path}/tools/copy_instance.sh"
